e589d9894778405f3b6c1af0b52d88ea517475f1
IAxFnsx <49125674+IAxFnsx@users.noreply.github.com>
2021-04-18 02:47:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client("NTIwNjkzODQzODA1NjAxODAx.YHtzLg.mwBc_ijcLYnGXecbojcqU3TNHRw");
 const adminprefix = "-"
 
 
